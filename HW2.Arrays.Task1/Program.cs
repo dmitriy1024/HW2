60123367841b081e60a01ff34a0fda36b7f5ca4c
dmitriy1024 <dfedoryshyn@gmail.com>
2015-08-20 00:28:44 +0300

    Arrays
    HW2 Arrays is ready

---
+++ HW2.Arrays.Task1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace HW2.Arrays.Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 1, 2, 3, 4, 5 };
+            Console.WriteLine("Original array:");
+            foreach (var item in array)
+                Console.Write(item + "  ");
+
+            Console.WriteLine();
+            int[] reversedArray = MyReverse(array);
+            Console.WriteLine("Reversed Array:");
+            foreach (var item in reversedArray)
+                Console.Write(item + "  ");
+
+            Console.ReadKey();
+        }
+
+        static int[] MyReverse(int[] arr)
+        {
+            int[] arrForInvert = new int[arr.Length];
+            arr.CopyTo(arrForInvert, 0);
+            Array.Reverse(arrForInvert);
+            return arrForInvert;
+        }
+    }
+}
