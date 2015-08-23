2d4af38f4e4fa4050ce0086b3d2dd700187aad8b
dmitriy1024 <dfedoryshyn@gmail.com>
2015-08-23 21:38:23 +0300

    HW2 methods
    HW2 methods

---
+++ HW2.Methods.Task1/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HW2.Methods.Task1
+{   
+    enum Currency{
+        USD = 0,
+        EUR = 1,
+        RUB = 2,
+        UAH = 3}
+
+    class Program
+    {
+        static Dictionary<Currency, double> usdollarExch = new Dictionary<Currency, double>();
+        
+        static Program()
+        {   
+            //инициализация курсов относительно доллара
+            //предположим, что курсы покупки и продажи одинаковы
+            usdollarExch.Add(Currency.USD, 1d);
+            usdollarExch.Add(Currency.EUR, 0.9057);
+            usdollarExch.Add(Currency.RUB, 65.7220);
+            usdollarExch.Add(Currency.UAH, 22.0662);
+        } 
+
+        static void Main(string[] args)
+        {
+            Currency originalCurrency;
+            Currency convertToCurrency;
+            string enteredString;
+            decimal total;
+
+            while(true)
+            {
+                Console.WriteLine("Enter the currensy like (USD, EUR, RUB, UAH)  (x to exit)");
+                enteredString = Console.ReadLine();
+                if (enteredString == "x")
+                    break;
+                if(!Enum.TryParse(enteredString, out originalCurrency))
+                {
+                    Console.WriteLine("Invalid currency!");
+                    continue;
+                }
+
+                Console.WriteLine("Enter the total -> (x to exit)");
+                enteredString = Console.ReadLine();
+                if (enteredString == "x")
+                    break;
+                if(!Decimal.TryParse(enteredString, out total))
+                {
+                    Console.WriteLine("Invalid total!");
+                    continue;
+                }
+
+                Console.WriteLine("Enter the currensy FOR CONVERT TO like (USD, EUR, RUB, UAH)  (x to exit)");
+                enteredString = Console.ReadLine();
+                if (enteredString == "x")
+                    break;
+                if (!Enum.TryParse(enteredString, out convertToCurrency))
+                {
+                    Console.WriteLine("Invalid currency!");
+                    continue;
+                }
+
+                Console.WriteLine(ConvertCurrency(originalCurrency, total, convertToCurrency));
+            }
+        }
+
+        static decimal ConvertCurrency(Currency origCurrency, decimal total, Currency convToCurrency)
+        {
+            double exchange = usdollarExch[origCurrency] * usdollarExch[convToCurrency];
+            return total * (decimal)exchange;            
+        }
+    }
+}
---
+++ HW2.Methods.Task1/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("HW2.Methods.Task1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("HW2.Methods.Task1")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Параметр ComVisible со значением FALSE делает типы в сборке невидимыми 
+// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("c199f361-a351-4f9a-b062-0c70ef865446")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
